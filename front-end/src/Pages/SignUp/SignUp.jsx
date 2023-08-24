0882f0ee3ee31cda3ecf6751249fcca33a439449
Ali <alihassan.mentorsol@gmail.com>
2023-08-24 22:42:50 +0500

    sign-up auth

--- front-end/src/Pages/SignUp/SignUp.jsx
+++ front-end/src/Pages/SignUp/SignUp.jsx
@@ -1,35 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignUp.scss";
+
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  //Name Validation
+  const nameValidation = () => {
+    if (name.trim() === "") {
+      alert("Name is Required")
+    }
+  }
+  //Email Validation
+  const emailValidation = () => {
+    if (!/^.+@.+\..+$/.test(email.trim())) {
+      alert("Enter Correct Email Adress")
+    }
+  }
+  //Password Validation
+  const passwordValidation = () => {
+    if (password.trim("") || password.length < 3 || !/\d/.test(password)                 // Check if it contains at least one digit
+    ) {
+      alert("Enter Valid password containing atleast one digit ")
+
+    }
+    if (!/^.+@.+\..+$/.test(email.trim())) {
+      alert("Enter Correct Email Adress")
+    }
+  }
+  //Submit Form
+  const submitForm = () => {
+    nameValidation()
+    emailValidation()
+  }
+
   return (
     <div>
       <div className="wrapper">
         <h1>Sign Up</h1>
         <div className="form">
           <div className="name">
-            <label for="name">Enter Your Name</label>:
-            <input type="text" id="name" placeholder="Name" />
+            <label htmlFor="name">Enter Your Name</label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Name"
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+            />
           </div>
           <div className="email">
-            <label for="email">Enter Email</label>:
-            <input type="email" id="email" placeholder="Email" />
+            <label htmlFor="email">Enter Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+            />
           </div>
           <div className="Password">
-            <label for="password">Enter Password</label>:
-            <input type="password" id="password" placeholder="Password" />
+            <label htmlFor="password">Enter Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
           </div>
           <div className="confirmPassword">
-            <label for="confirpassword">Enter Password</label>:
+            <label htmlFor="confirmPassword">Confirm Password</label>
             <input
-              type="confirmpassword"
-              id="password"
+              type="password"
+              id="confirmPassword"
               placeholder="Confirm Password"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
             />
           </div>
           <div className="submitForm">
-            <button>Submit Form</button>
+            <button className="submit-btn" onClick={submitForm}>Submit Form</button>
           </div>
-
         </div>
       </div>
     </div>
